32dd4863a8a20173a2cab2897ac6baeff8e7bc7f
LeeKann <ekannampuzha@cpp.edu>
2017-09-27 22:27:14 -0700

    EchoServer and EchoClient working.

--- EchoClient.java
+++ EchoClient.java
@@ -1,6 +1,9 @@
 
 import java.io.InputStream;
 import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.PrintStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
@@ -11,7 +14,19 @@ public final class EchoClient {
             InputStream is = socket.getInputStream();
             InputStreamReader isr = new InputStreamReader(is, "UTF-8");
             BufferedReader br = new BufferedReader(isr);
-            System.out.println(br.readLine());
+            //System.out.println(br.readLine());
+            OutputStream os = socket.getOutputStream();
+            PrintStream out = new PrintStream(os, true, "UTF-8");
+            //out.printf("This is my message\n");
+            while(true) {
+                System.out.print("Client> ");
+                InputStreamReader input = new InputStreamReader(System.in, "UTF-8");
+                BufferedReader binput = new BufferedReader(input);
+                out.printf(binput.readLine() + "\n");
+                String line;
+                line = br.readLine();
+                System.out.println("Server> " + line);
+            }
         }
     }
 }
--- EchoServer.java
+++ EchoServer.java
@@ -1,6 +1,9 @@
 
 import java.io.OutputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,7 +17,18 @@ public final class EchoServer {
                     System.out.printf("Client connected: %s%n", address);
                     OutputStream os = socket.getOutputStream();
                     PrintStream out = new PrintStream(os, true, "UTF-8");
-                    out.printf("Hi %s, thanks for connecting!%n", address);
+                    //out.printf("Hi %s, thanks for connecting!%n", address);
+                
+                    InputStream is = socket.getInputStream();
+                    InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+                    BufferedReader br = new BufferedReader(isr);
+                    String line;
+                    while((line = br.readLine()) != null) {
+                        out.printf(line + "\n");
+                        //System.out.println(line);
+                    //out.flush();
+                    }
+                    System.out.printf("Client disconnected: %s%n", address);
                 }
             }
         }
